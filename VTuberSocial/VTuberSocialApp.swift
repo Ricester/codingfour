f976e0f89a5af78907cae1cac0b5a4598c948daa
Erika Imanaka Yu <erika@(null).lan>
2023-06-05 11:02:19 +0100

    Initial Commit

---
+++ VTuberSocial/VTuberSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTuberSocialApp.swift
+//  VTuberSocial
+//
+//  Created by Erika Imanaka Yu on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct VTuberSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
